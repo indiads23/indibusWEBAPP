f8d9d968febaca1272342a6c795c5979ac54b6b8
1saurabh2003 <sy108006@gmail.com>
2023-12-24 17:41:49 +0530

    footer by Saurabh

--- indibuswebapp/src/components/Footer.jsx
+++ indibuswebapp/src/components/Footer.jsx
@@ -6,17 +6,29 @@ const Footer = () => {
     
       <div className='footer'>
         <div className='footerContainer'>
-          <div className='footerContent'>
+          <div className='row'>
+            <div className='footerContent'>
               <form action="">
-                <textarea name="" id="" placeholder='FeedBack' cols="30" rows="3"></textarea>
+                <textarea name="" id="" placeholder='FeedBack' cols="30" rows="5"></textarea>
                 <button type='Submit'> Submit </button>
               </form>
-          </div>
-          <div className='footerContent'>
-
-          </div>
-          <div className='footerContent'>
-
+            </div>
+            <div id='logocontainer' className='footerContent'>
+                <img className='footerlogo' src="./xlogo.png" alt="xlogo" />
+                <img className='footerlogo' src="./facebooklogo.png" alt="xlogo" />
+                <img className='footerlogo' src="./instalogo.png" alt="xlogo" />
+                <img className='footerlogo' src="./gmaillogo.png" alt="xlogo" />
+            </div>
+            <div  id="footname" className='footerContent'>
+              <div className='indi'>
+                <p>
+                  <span id='in'>In</span>
+                  <span id='di'>Di</span>
+                  <span id='bus'>Bus</span>
+                  <span id='di'> 2023</span>
+                </p>
+                </div>
+            </div>
           </div>
         </div>
       </div>
