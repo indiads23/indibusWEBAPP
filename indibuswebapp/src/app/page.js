a8d8037acae68a5e81afa138e328da66772a6cd9
1saurabh2003 <sy108006@gmail.com>
2023-12-29 00:18:31 +0530

    Created Middiv by Saurabh

--- indibuswebapp/src/app/page.js
+++ indibuswebapp/src/app/page.js
@@ -1,10 +1,13 @@
 import Updiv from '@/components/Updiv'
+import Middiv from '@/components/Middiv'
 import styles from './page.module.css'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Updiv/>
+      <Middiv/>
     </main>
   )
 }
---
+++ indibuswebapp/src/components/Middiv.jsx
@@ -0,0 +1,52 @@
+'use client'
+import React from 'react'
+import styles from "../app/page.module.css"
+
+
+const Middiv = () => {
+  return (
+    <div>
+
+    
+    <div className={styles.maincontainer}>
+        <div className={styles.white1}>
+        <div className={styles.midcontainer1}>
+            <div className={styles.midimg1}>
+                <img src="./who are we 1.png" alt="" />
+            </div>
+            <div className={styles.midtext1}>
+                <h2 className={styles.h2w} >WHO ARE WE</h2>
+                <p className={styles.pw} >we envision a future where businesses thrive in the digital landscape with unparalleled success. 
+                    Our company stands as a beacon of innovation and expertise, leading the way in the realm of digital 
+                    business solutions. As the trusted partner of choice for businesses worldwide, INDIBUS is committed 
+                    to delivering excellence in content creation, digital marketing, website development, and digital 
+                    advertising. Our vision is to empower businesses with the tools and strategies they need to not 
+                    only survive but to excel in the ever-evolving digital ecosystem.  </p>
+            </div>
+        </div>
+        </div>
+
+        <div className={styles.white}>
+        <div className={styles.midcontainer2}>
+            <div className={styles.midtext2}>
+                <h2 className={styles.h2w} >ROLE OF SOCIAL MEDIA</h2>
+                <p className={styles.pw} >Creating and developing a website is important for any business. Along with having a website, 
+                    you need to expand your online outreach to social media platforms. Social networking sites such 
+                    as Instagram and Facebook are essential for maintaining a competitive edge. Companies that do 
+                    not have active social media accounts risk missing out on numerous marketing opportunities.Social 
+                    media eases the process of providing and receiving feedback. If your customers have concerns or 
+                    problems with what your business offers, they can let you    </p>
+            </div>
+            <div className={styles.midimg2}>
+                <img src="./role of SM 1.png" alt="" />
+            </div>
+        </div>
+        </div>
+    </div>    
+
+
+    </div>
+  )
+}
+
+export default Middiv
