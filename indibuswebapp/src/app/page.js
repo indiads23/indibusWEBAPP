1ab819c11432d71789100a9f89ea43dcec7d2326
1saurabh2003 <sy108006@gmail.com>
2023-12-29 20:24:15 +0530

    Added AOS in Middiv by Saurabh

--- indibuswebapp/src/app/page.js
+++ indibuswebapp/src/app/page.js
@@ -1,9 +1,19 @@
+'use client'
 import Updiv from '@/components/Updiv'
 import Middiv from '@/components/Middiv'
 import styles from './page.module.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 
 export default function Home() {
+  useEffect(()=>{
+    AOS.init({
+      duration:1200,
+      once:false
+    },[])
+  })
   return (
     <main className={styles.main}>
       <Updiv/>
