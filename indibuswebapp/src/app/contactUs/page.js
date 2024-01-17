d38c3b4c598157e34ba9f72e26f03574dd182906
1saurabh2003 <sy108006@gmail.com>
2024-01-17 22:00:07 +0530

    Created Contact page by saurabh

--- indibuswebapp/src/app/contactUs/page.js
+++ indibuswebapp/src/app/contactUs/page.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import styles from "../page.module.css"
+import Location from '@/components/location'
+import Contactform from '@/components/Contactform'
+
+
+
 const ContactPage = () => {
   return (
-    <div className={styles.ContactMainDiv}>
-      <div className={styles.contentHeading}><h1> CONTACTS </h1></div>
+    <div>
+      <div className={styles.contmain}>
+          <div className={styles.contformcontainer}>
+            <div className={styles.contform}>
+                <h1 className={styles.conth1} >Get in <span className={styles.touch}>Touch</span></h1>
+                <p className={styles.formp} >Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
+                <Contactform/>
+                 <div className={styles.conticon}>
+                    <div className={styles.contphone}>
+                       <img src="./phoneiconbw.png" alt=""/>
+                      <p>+91 93100 09063</p>
+                    </div>
+                    <div className={styles.contphone}>
+                       <img src="./emailbw.png" alt=""/>
+                      <p>indibus2024@gmail.com</p>
+                    </div>
+                   
+                </div>
+            </div>
+          </div>
+          <div data-aos="fade-left"
+     data-aos-anchor="#example-anchor"
+     data-aos-offset="500"
+     data-aos-duration="3000" className={styles.location}>
+            <Location/>
+          </div>
+      </div>
     </div>
   )
 }
---
+++ indibuswebapp/src/components/Location.jsx
@@ -0,0 +1,21 @@
+// components/GoogleMapsLocation.js
+import React from 'react';
+
+const GoogleMapsLocation = () => {
+  return (
+    <div>
+      <iframe
+        title="Google Maps Location"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.8745434286457!2d77.44225918794551!3d28.63352192130062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef5cb9683d7f%3A0x541c1ae70e1d2c7e!2sINDIBUS!5e0!3m2!1sen!2sin!4v1705490889386!5m2!1sen!2sin"
+        width="450"
+        height="600"
+        style={{ border: '0' }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default GoogleMapsLocation;
