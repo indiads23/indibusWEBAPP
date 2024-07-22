6530f7806fe37fc9f6d2b7c7dbe5d70ddf6a8521
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-07-22 16:31:53 +0530

    responsive footer

--- indibuswebapp/src/components/Footer.jsx
+++ indibuswebapp/src/components/Footer.jsx
@@ -1,75 +1,57 @@
-'use client'
 import React from 'react'
-import { SocialIcon } from 'react-social-icons'
-import Link from 'next/link'
+import "../app/footer.css"
+import { BsInstagram, BsLinkedin, BsTwitch, BsTwitter } from 'react-icons/bs'
+import { GrGoogle } from 'react-icons/gr'
 
-const Footer = () => {
-  const date = new Date()
-  const year = date.getFullYear()
+export default function Footer() {
   return (
- 
-        <div className='footer'>
-          <div className='smallfoot'>
-            <div className='footrow'>
-                <div className='col4'>
-                <Link href="/" style={{color:"#FFFFFF"}}>
-        <div className="logo">
-            <img className='logoImage'
-                src='/newlogo.jpg'
-            />
+    <footer class="footer">
+      <div class="container">
+        <div className="footer-logo">
+          <img className='logoImage'
+            src='/newlogo.jpg'
+          />
         </div>
-        </Link>
-                </div>
-                <div className='col4a'>
-                   <h3>Contacts</h3>
-                   <div className='contacts'>
-                    <img className='phicon' src="./phone.svg" alt="phone" />
-                      <div className='number'>
-                        <p>+91 93100 09063</p>
-                        <p>+91 82998 97560</p> 
-                      </div>
-                   </div>
-                   <div className='location'>
-                    <img className='phicon' src="./location.svg" alt="location" />
-                      <div className='number'>
-                        <p>19 KM stone,ABES IT, First Floor</p>
-                      </div>
-                   </div>
-
-                </div>
-                <div id='company' className='col4a'>
-                   <h3>Company</h3>
-                   <ul>
-                    <li><a className='footlink' href='#'>HOME</a></li>
-                    <li><a className='footlink' href='#'>SERVICES</a></li>
-                    <li><a className='footlink' href='/contactUs'>CONTACTS</a></li>
-                   </ul>
-                </div>
-                {/* <div id='feedback' className='col4a'>
-                   <h3>Feedback</h3>
-                   <div className='feed'>
-                        <textarea id="story" name="story" rows="6" cols="28">
-
-                        </textarea>
-                        <button className='btnicon' ><img className='sendicon' src="./send.svg" alt="send" /></button>
-                    </div>
-                </div> */}
-              </div>
-            </div> 
-            <hr />
-               <div id='logocontainer' className='footerContent'>
-                <SocialIcon id='facebook' className='footerlogo' url='https://www.facebook.com/indibus23?mibextid=gik2fB'/>
-                <SocialIcon id='x' className='footerlogo'  url='https://x.com/indibus23?t=WDqQVpPbmi0QF2m0fXmHGg&s=08'/>
-                <SocialIcon id='instagram' className='footerlogo' url='https://www.instagram.com/indibus23?igsh=MXN5aGQ4YW9vcTU4aA=='/>
-                <SocialIcon id='linkedin' className='footerlogo' url='https://www.linkedin.com/company/indibus23/'/>
-              </div>
-              <div className='copyright'>
-                <p id='copy' >&#9400;INDIBUS {year}</p>
-              </div>
+        <div class="row">
+          <div class="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li><a href="#">about us</a></li>
+              <li><a href="#">our services</a></li>
+              <li><a href="#">privacy policy</a></li>
+              <li><a href="#">affiliate program</a></li>
+            </ul>
           </div>
-
-   
+          <div class="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">shipping</a></li>
+              <li><a href="#">returns</a></li>
+              <li><a href="#">order status</a></li>
+              <li><a href="#">payment options</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>online shop</h4>
+            <ul>
+              <li><a href="#">watch</a></li>
+              <li><a href="#">bag</a></li>
+              <li><a href="#">shoes</a></li>
+              <li><a href="#">dress</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+              <a href="#"><BsInstagram /></a>
+              <a href="#"><BsTwitter /></a>
+              <a href="#"><GrGoogle/></a>
+              <a href="#"><BsLinkedin /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
-
-export default Footer
