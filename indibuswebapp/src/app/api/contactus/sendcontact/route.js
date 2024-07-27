63e25d221bc12ba27d63757158ed56819cd8ce14
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2024-07-27 21:44:28 +0530

    test

--- indibuswebapp/src/app/api/contactus/sendcontact/route.js
+++ indibuswebapp/src/app/api/contactus/sendcontact/route.js
@@ -5,25 +5,32 @@ const { NextResponse } = require("next/server");
 
 connect()
 export async function POST(request){
-    const reqBody =await request.json()
-    const {name,email,phoneNumber,interest,message} = reqBody
-    console.log(name,email,phoneNumber,interest,message);
-    console.log(reqBody);
-
-    const newContact = new Contact({
-        name:name,
-        email:email,
-        phoneNumber:phoneNumber,
-        interest:interest,
-        message:message
-    })
-
-    const savedContactDetails = await newContact.save()
-
-    return NextResponse
-    .json({
-        message:"Message Recorded Successfully",
-        success:true,
-        savedContactDetails
-    })
+   try {
+     const reqBody =await request.json()
+     const {name,email,phoneNumber,interest,message} = reqBody
+     console.log(name,email,phoneNumber,interest,message);
+     console.log(reqBody);
+ 
+     const newContact = new Contact({
+         name:name,
+         email:email,
+         phoneNumber:phoneNumber,
+         interest:interest,
+         message:message
+     })
+ 
+     const savedContactDetails = await newContact.save()
+ 
+     return NextResponse
+     .json({
+         message:"Message Recorded Successfully",
+         success:true,
+         savedContactDetails
+     })
+   } catch (error) {
+     return NextResponse.json({
+        message:error.message,
+        success:false
+     })
+   }
 }
