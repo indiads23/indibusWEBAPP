09d185777781fcbfacb747dd9382ff114b6e1725
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2024-07-27 19:10:03 +0530

    Added functionality of Contactus by Ganesh

---
+++ indibuswebapp/src/models/contact.model.js
@@ -0,0 +1,27 @@
+import mongoose,{Schema} from "mongoose";
+
+
+const contactSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true
+    },
+    interest:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+})
+
+export const Contact = mongoose.models.Contact||mongoose.model("Contact",contactSchema)
