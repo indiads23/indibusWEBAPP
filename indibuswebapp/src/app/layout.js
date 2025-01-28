499623faddc5f6b91b68e6a2237865afbc334fdf
indiads23 <153978194+indiads23@users.noreply.github.com>
2025-01-29 00:17:49 +0530

    Update layout.js

--- indibuswebapp/src/app/layout.js
+++ indibuswebapp/src/app/layout.js
@@ -14,24 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-            `,
-          }}
-        />
-      </head>
+      
 
       <body className={inter.className}>
         <Navbar/>
